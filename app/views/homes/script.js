9804b9774d18c1a63dfcb74e72c962fad5a7c36a
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-15 05:58:24 +0000

    １２章修了、２時間後再度チャレンジ

--- app/views/homes/script.js
+++ app/views/homes/script.js
@@ -1,10 +1,10 @@
-const swiper = new Swiper('.swiper', {
-  loap: true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-});
+// const swiper = new Swiper('.swiper', {
+//   loap: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   }
+// });
