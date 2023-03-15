257607bfe57a4bdd95841f83c9e2aa7b665b4dcd
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-15 05:23:15 +0000

    １１章修了

--- app/views/homes/js/script.js
+++ app/views/homes/js/script.js
@@ -23,13 +23,26 @@
 // });
 
 // ハンバーガーメニュー
-$(function() {
-  $('.menu-trigger').on('click', function(e) {
-    // .menu-triggerがクリックされたときに、処理が行われる
-    $(this).toggleClass("active");
-    // クリックされた要素自身にactiveクラスが追加され、もう一度クリックすると削除されます。
-    $('#sp-menu').fadeToggle();
-    // .fadeToggle()は、要素のフェードイン・フェードアウトを切り替える
-    e.preventDefault()
-  })
+// $(function() {
+//   $('.menu-trigger').on('click', function(e) {
+//     // .menu-triggerがクリックされたときに、処理が行われる
+//     $(this).toggleClass("active");
+//     // クリックされた要素自身にactiveクラスが追加され、もう一度クリックすると削除されます。
+//     $('#sp-menu').fadeToggle();
+//     // .fadeToggle()は、要素のフェードイン・フェードアウトを切り替える
+//     e.preventDefault()
+//   })
+// })
+
+// タブメニュー
+$('#tab-contents .tab[id != "tab1"]').hide();
+// tab-contentsの子要素のclassがtabでidがtab1でないものはすべて隠す
+$('#tab-menu a').on('click', function(event) {
+  // タブメニューのaタグがクリックされたら
+  $('#tab-contents .tab').hide();
+  // タブコンテンツのタブがすべて非表示に
+  $('#tab-menu .active').removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 })
