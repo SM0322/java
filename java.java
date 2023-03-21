46e068643410ed2c401d2cf57684baabbb50a740
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-21 05:27:01 +0000

    九九

--- java.java
+++ java.java
@@ -249,3 +249,15 @@ public class hello {
     }
   }
 }
+// 九九の計算
+public class hello {
+  public static void main(String []args) {
+    for (int i = 1; i < 10; i++) {
+      for (int j = 1; j < 10; j++) {
+        System.out.println(i * j);
+        System.out.println(" ");
+      }
+    System.out.println("");  
+    }
+  }
+}
