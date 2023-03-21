0a8f5e1fa484df34ccfe9aeb5e0224793fe3b641
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-21 06:06:09 +0000

    課題３終了

--- java.java
+++ java.java
@@ -283,3 +283,71 @@ public class hello {
     }
   }
 }
+// 3-3
+public class hello {
+
+  public static void main(String []args) {
+    int isHungry = 1;
+    String food = "カレー";
+    System.out.println("こんにちは");
+    if (isHungry == 0) {
+      System.out.println("おなかがいっぱいです");
+    } else {
+      System.out.println("腹ペコです");
+      System.out.println(food + "をいただきます");
+    }
+    System.out.println("ごちそうさまでした");
+  }
+}
+// 3-4
+public class hello {
+  public static void main(String []args) {
+    boolean tenki =true;
+    if (tenki == true) {
+      System.out.println("洗濯");
+      System.out.println("散歩");
+    } else {
+      System.out.println("DVD");
+      System.out.println("寝る");
+    }
+  }
+}
+// 3-5
+public class hello {
+  public static void main(String []args) {
+    System.out.print("[メニュ-] 1:検索 2:登録 3:削除 4:変更");
+    int selected = new java.util.Scanner(System.in).nextInt();
+    switch (selected) {
+      case 1:
+        System.out.println("検索します");
+        break;
+      case 2:
+        System.out.println("登録します");
+        break;
+      case 3:
+        System.out.println("削除します");
+        break;
+      case 4:
+        System.out.println("変更します");
+        break;
+    }
+  }
+}
+// 3-6
+public class hello {
+  public static void main(String []args) {
+    System.out.print("[数当てゲーム]");
+    int ans = new java.util.Random().nextInt(1);
+    for (int i = 0; i < 5; i++) {
+      System.out.println("0~9の数字を入力してください");
+      int num = new java.util.Scanner(System.in).nextInt();
+      if (num == ans) {
+        System.out.println("アタリ！");
+        break;
+      } else {
+        System.out.println("ハズレ！");
+      }
+    }
+    System.out.println("ゲームを終了します");
+  }
+}
