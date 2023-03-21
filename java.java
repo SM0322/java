2eac21da5d7a3e0e417268677b180eeb5c4a7856
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-21 01:38:36 +0000

    Math.max()

--- java.java
+++ java.java
@@ -71,3 +71,13 @@ public class hello {
     System.out.print(message);
   }
 }
+// Math.max()で大きい方の数字を代入
+public class hello {
+
+  public static void main(String []args) {
+    int a = 5;
+    int b = 3;
+    int m = Math.max(a, b);
+    System.out.println(m);
+  }
+}
