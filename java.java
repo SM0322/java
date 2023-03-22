d917d9008a4281f4158867e4e79ad5a7d6651520
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-22 01:21:23 +0000

    配列とifの組み合わせ

--- java.java
+++ java.java
@@ -428,3 +428,16 @@ public class hello {
     System.out.println(average);
   }
 }
+// 配列とifの組み合わせ
+public class hello {
+  public static void main(String []args) {
+    int [] scores = new int[] {20, 30, 40, 50, 80};
+    int count = 0;
+    for (int i = 0; i < scores.length; i++) {
+      if (scores[i] >= 50) {
+          count++;
+      }
+    }
+    System.out.println(count);
+  }
+}
