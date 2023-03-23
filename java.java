499c91700885cdb850d97c871d343910bc2d9b74
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-23 23:37:14 +0000

    mainメソッドから必ず動くことを確認

--- java.java
+++ java.java
@@ -530,3 +530,16 @@ public class Main {
     System.out.println("こんにちは");
   }
 }
+// mainメソッドから必ず動くことを確認
+public class Main {
+  public static void methodA() {
+    System.out.println("methodA");
+    methodB();
+  }
+  public static void methodB() {
+    System.out.println("methodB");
+  }
+  public static void main(String []args) {
+    methodA();
+  }
+}
